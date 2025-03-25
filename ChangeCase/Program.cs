4704222703b1356320a502813f70066eb4c94e39
professionalcoder123 <107976195+professionalcoder123@users.noreply.github.com>
2025-03-25 14:25:23 +0530

    Add files via upload

---
+++ ChangeCase/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChangeCase
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the name of the place you like to visit");
+            string place=Console.ReadLine();
+            Console.WriteLine("Uppercase : "+place.ToUpper());
+            Console.WriteLine("Lowercase : " + place.ToLower());
+        }
+    }
+}
